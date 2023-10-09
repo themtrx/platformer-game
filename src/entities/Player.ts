e17cdcf9d72ac5228b4e829403416bca6c3dbaaa
themtrx <themtrx@mail.bg>
2023-10-09 13:17:45 +0300

    Player jump

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -8,6 +8,9 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     gravity: number = 500
     playerSpeed: number = 200
+
+    jumpCount: number = 0
+    consecutiveJumps: number = 1
     
     constructor(scene: GameScene, x: number, y: number){
         super(scene, x, y, 'player')
@@ -33,8 +36,10 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     }
 
     update() {
-        const { left, right } = this.cursors
-
+        const { left, right, space } = this.cursors
+        const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space)
+        const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor()
+  
         if(left.isDown){
             this.setVelocityX(-this.playerSpeed)
             this.setFlipX(true)
@@ -44,6 +49,13 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
         }else {
             this.setVelocityX(0)
         }
+
+        if((isSpaceJustDown) && (onFloor || this.jumpCount < this.consecutiveJumps)){
+            this.setVelocityY(-this.playerSpeed * 1.5)
+            this.jumpCount++
+        }
+
+        if(onFloor) this.jumpCount = 0
 
         this.body.velocity.x != 0 ?
             this.play('run', true) : this.play('idle', true)
