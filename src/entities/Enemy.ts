5b68800180f016af4b0fecf0732d0a2b5705fc8f
themtrx <themtrx@mail.bg>
2023-10-10 13:51:26 +0300

    Added patrol distance

--- src/entities/Enemy.ts
+++ src/entities/Enemy.ts
@@ -8,6 +8,8 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
     gravity: number = 500
     speed: number = 50
     timeFromLastTurn: number = 0
+    maxPatrolDistance: number = 200
+    currentPatrolDistance: number = 0
 
     rayGraphics: Phaser.GameObjects.Graphics
     setPlatformColliderLayer: Phaser.Tilemaps.TilemapLayer
@@ -44,15 +46,21 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
     }
 
     update(time: number, delta: number) {
+
+        this.currentPatrolDistance += Math.abs(this.body.deltaX())
+
         const { ray, hasHit } = this.raycast(this.body as Phaser.Physics.Arcade.Body, this.setPlatformColliderLayer, 30, 2)
         
         // Check if the ray has hit platform
         // and wait for 100ms before turning
         // to avoid turning too quickly
-        if(!hasHit && this.timeFromLastTurn + 100 < time){
+        if((!hasHit || this.currentPatrolDistance >= this.maxPatrolDistance) &&
+            this.timeFromLastTurn + 100 < time){
+
             this.setFlipX(!this.flipX)
             this.setVelocityX(this.speed = -this.speed)
             this.timeFromLastTurn = time
+            this.currentPatrolDistance = 0
         }
         
         this.rayGraphics.clear()
