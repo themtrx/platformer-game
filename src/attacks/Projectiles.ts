46a2ce28fbf08ecaaea928fe2d49c9f056696902
themtrx <themtrx@mail.bg>
2023-10-11 17:15:18 +0300

    Add projectile cooldown

--- src/attacks/Projectiles.ts
+++ src/attacks/Projectiles.ts
@@ -1,8 +1,10 @@
 import { GameScene } from "../Scenes/GameScene"
 import Player from "../entities/Player"
 import Projectile from "./Projectile"
+import { getTimestamp } from "../utils/functions"
 
 export default class Projectiles extends Phaser.Physics.Arcade.Group {
+    timeFromLastProjectile: Number
 
     constructor(scene: GameScene) {
         super(scene.physics.world, scene)
@@ -21,9 +23,12 @@ export default class Projectiles extends Phaser.Physics.Arcade.Group {
 
         if(!projectile) return
 
+        if(this.timeFromLastProjectile && 
+            this.timeFromLastProjectile + projectile.cooldown > getTimestamp()) return
+
         const center = initiator.getCenter()
         let centerX = center.x
-        
+
         if(initiator.lastDirection == Phaser.Physics.Arcade.FACING_RIGHT) {
             projectile.speed = Math.abs(projectile.speed)
             projectile.setFlipX(false)
@@ -35,5 +40,6 @@ export default class Projectiles extends Phaser.Physics.Arcade.Group {
         }
 
         projectile.fire(centerX, center.y)
+        this.timeFromLastProjectile = getTimestamp()
     }
 }
