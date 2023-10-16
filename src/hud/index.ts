8d17efd741fb9aa35083b77d51f3caf3a24fcb9f
themtrx <themtrx@mail.bg>
2023-10-16 17:15:09 +0300

    Add other item in board container

--- src/hud/index.ts
+++ src/hud/index.ts
@@ -3,20 +3,33 @@ import { GameScene } from "../Scenes/GameScene"
 export default class Hud extends Phaser.GameObjects.Container {
     scene: GameScene
 
+    containerWidth: number = 50
+    fontSize: number = 20
+    color: string = '#fff'
+    scoreBoard: Phaser.GameObjects.GameObject
+
     constructor(scene: GameScene, x: number, y: number) {
         super(scene, x, y)
         this.scene = scene
         this.scene.add.existing(this)
 
-        this.setPosition(this.scene.rigthTopCorner.x -100, this.scene.rigthTopCorner.y + 10)
+        this.setPosition(this.scene.rigthTopCorner.x - this.containerWidth, this.scene.rigthTopCorner.y + 10)
         this.setScrollFactor(0)
 
         this.setupList()
     }
 
     setupList() {
-        const scoreBoard = this.scene.add.text(0, 0, 'Score: 0', {fontSize: '20px', color: '#fff'})
-        
-        this.add(scoreBoard)
+        this.scoreBoard = this.createScoreBoard()
+        this.add(this.scoreBoard)
+    }
+
+    createScoreBoard() {
+        const scoreText = this.scene.add.text(0, 0, '0', {fontSize: `${this.fontSize}px`, color: this.color})
+        const scoreImg = this.scene.add.image(scoreText.width + 5, scoreText.y, 'diamond').setScale(1.2).setOrigin(0)
+
+        const scoreBoard = this.scene.add.container(0, 0, [scoreText, scoreImg])
+
+        return scoreBoard
     }
 }
